941a244fbfd7665d2b33b6a76c42bb162d811da2
NocDevX <xdev.noc@gmail.com>
2025-06-09 13:22:45 -0300

    #1 - Commit Inicial

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Heading } from './components/Heading';
+
+import './styles/theme.css';
+import './styles/global.css';
+
+export function App() {
+    return (
+        <>
+            <Heading>Header</Heading>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum
+                cumque incidunt accusamus commodi quasi sit, doloribus in
+                fugiat, repellat laudantium voluptas praesentium labore dolore
+                unde! Aliquam, dolorem! A, quia rerum!
+            </p>
+        </>
+    );
+}
